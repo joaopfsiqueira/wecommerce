dbbb1556447d7b331ca71a409fcdf6ef6ba355a8
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-07-29 17:02:02 -0300

    feat: separing components in divs

--- src/App.js
+++ src/App.js
@@ -9,15 +9,26 @@ function App() {
       </header>
       <main>
         <h1>Featured Products</h1>
+        <div className="products">
         {
           data.products.map(product => (
-            <div>
+            <div className="product" key={product.slug}>
+            <a href={`/product/${product.slug}`}>
             <img src={product.image} alt={product.name} />
-            <p>{product.name}</p>
-            <p>{product.price}</p>
+            </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                <p>{product.name}</p>
+                </a>
+                <p>
+                <strong>${product.price}</strong>
+                </p>
+              <button>Add to cart</button>
+              </div>
             </div>
           ))
         }
+        </div>
       </main>
     </div>
   );
