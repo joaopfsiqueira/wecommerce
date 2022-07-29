e1395ff26550dc95669d7973f50d80e0b13e50eb
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-07-29 17:28:30 -0300

    feat: separing home screen

--- src/App.js
+++ src/App.js
@@ -1,37 +1,26 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import data from './data';
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
   return (
+    <BrowserRouter>
     <div>
       <header >
         <a href="/">Amazon</a>
       </header>
+
       <main>
-        <h1>Featured Products</h1>
-        <div className="products">
-        {
-          data.products.map(product => (
-            <div className="product" key={product.slug}>
-            <a href={`/product/${product.slug}`}>
-            <img src={product.image} alt={product.name} />
-            </a>
-              <div className="product-info">
-                <a href={`/product/${product.slug}`}>
-                <p>{product.name}</p>
-                </a>
-                <p>
-                <strong>${product.price}</strong>
-                </p>
-              <button>Add to cart</button>
-              </div>
-            </div>
-          ))
-        }
-        </div>
+        <Routes>
+          <Route path="/" element={<HomeScreen/>} />
+        </Routes>
       </main>
+
     </div>
+    </BrowserRouter>
   );
+
 }
 
 export default App;
