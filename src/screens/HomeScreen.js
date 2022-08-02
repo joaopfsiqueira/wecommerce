b9b6f2151e9e1bab7bb8290c9484bf11ce9c3512
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-08-02 15:53:29 -0300

    feat: return data in front of back

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,26 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom'
-import data from "../data";
+import axios from 'axios';
 
 function HomeScreen() {
+
+  //connecting to the server to get the data
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/api/products');
+      setProducts(result.data);
+
+    };
+    fetchData();
+  }, []);
+
+
     return <div>
         <h1>Featured Products</h1>
         <div className="products">
         {
-          data.products.map(product => (
+          products.map(product => (
             <div className="product" key={product.slug}>
             <Link to={`/product/${product.slug}`}>
             <img src={product.image} alt={product.name} />
