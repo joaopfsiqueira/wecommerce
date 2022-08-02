2a7cb095e9400dcef530007604440b9257ccea34
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-08-02 18:22:14 -0300

    fix: fix bug that cause rating half star not showing

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -8,8 +8,8 @@ function Rating (props) {
                         rating >= 1 
                         ? 'fas fa-star' 
                         : rating >= 0.5 
-                        ? 'fas fa-star-half-o' 
-                        : 'far fa-star-o'}
+                        ? 'fas fa-star-half-alt' 
+                        : 'far fa-star'}
                 />
             </span>
             <span>
@@ -18,8 +18,8 @@ function Rating (props) {
                         rating >= 2
                         ? 'fas fa-star' 
                         : rating >= 1.5 
-                        ? 'fas fa-star-half-o' 
-                        : 'far fa-star-o'}
+                        ? 'fas fa-star-half-alt' 
+                        : 'far fa-star'}
                 />
             </span>
             <span>
@@ -28,8 +28,8 @@ function Rating (props) {
                         rating >= 3
                         ? 'fas fa-star' 
                         : rating >= 2.5 
-                        ? 'fas fa-star-half-o' 
-                        : 'far fa-star-o'}
+                        ? 'fas fa-star-half-alt' 
+                        : 'far fa-star'}
                 />
             </span>
             <span>
@@ -38,8 +38,8 @@ function Rating (props) {
                         rating >= 4
                         ? 'fas fa-star' 
                         :rating >= 3.5 
-                        ? 'fas fa-star-half-o' 
-                        : 'far fa-star-o'}
+                        ? 'fas fa-star-half-alt' 
+                        : 'far fa-star'}
                 />
             </span>
             <span>
@@ -48,8 +48,8 @@ function Rating (props) {
                         rating >= 5
                         ? 'fas fa-star' 
                         : rating >= 4.5 
-                        ? 'fas fa-star-half-o' 
-                        : 'far fa-star-o'}
+                        ? 'fas fa-star-half-alt' 
+                        : 'far fa-star'}
                 />
             </span>
         </div>
