a78a544153942157d585e3253a07cd87d32f969c
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-08-02 16:17:17 -0300

    feat: chaging useState for useReducer

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,15 +1,41 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer, useState } from 'react';
 import { Link } from 'react-router-dom'
-import axios from 'axios';
+import axios from 'axios'; 
+import logger from 'use-reducer-logger'
+
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return {...state, products: action.payload, loading: false};
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload};
+    default:
+      return state;      
+  }
+}
+
 
 function HomeScreen() {
-
+  const [{loading, error, products}, dispatch] = useReducer(logger(reducer), {
+    products: [],
+    loading: true,
+    error: ''
+  }) 
   //connecting to the server to get the data
-  const [products, setProducts] = useState([]);
+  // const [products, setProducts] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
+      dispatch({type: 'FETCH_REQUEST'});
+      try {
       const result = await axios.get('/api/products');
-      setProducts(result.data);
+      dispatch({type: 'FETCH_SUCCESS', payload: result.data});
+      // setProducts(result.data);
+    } catch (err) {
+      dispatch({type: 'FETCH_FAIL', payload: err.message});
+    }
 
     };
     fetchData();
